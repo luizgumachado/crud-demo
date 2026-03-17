a7d4a7245d5c0f997383f0b25fadcc067d039bab
Luiz Gustavo Souza <luizgustavo.gumachado@gmail.com>
2026-03-17 18:49:15 -0300

    feat: add localStorage data persistence

--- src/app/customer-service.ts
+++ src/app/customer-service.ts
@@ -5,7 +5,26 @@ import { Customer } from './register/customer';
   providedIn: 'root',
 })
 export class CustomerService {
+  static REPO_CUSTOMERS = "_CUSTOMERS";
+
   save(customer: Customer) {
-    console.log("Customer Data: " + customer);
+    const storage = this.getStorage();
+
+    storage.push(customer);
+    localStorage.setItem(CustomerService.REPO_CUSTOMERS, JSON.stringify(storage));
+    console.log("Saved succesfully. New Customer Data: " + customer);
+  }
+
+  getStorage() : Customer[] {
+    const customerRepository = localStorage.getItem(CustomerService.REPO_CUSTOMERS);
+
+    if(customerRepository) {
+      const customers = JSON.parse(customerRepository);
+      return customers;
+    }
+
+    const customers: Customer[] = [];
+    localStorage.setItem(CustomerService.REPO_CUSTOMERS, JSON.stringify(customers));
+    return customers;
   }
 }
